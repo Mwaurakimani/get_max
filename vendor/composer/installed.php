2ca842c252bac8317b30a2fd54bd8f4f9408ec91
MwauraKimani <kimmwaus@gmail.com>
2022-06-05 18:20:13 +0300

    com

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e81e0f3c79535c748522b84d52ed72a5200b239a',
+        'name' => 'mwaurakimani/user',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mwaurakimani/user' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e81e0f3c79535c748522b84d52ed72a5200b239a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
